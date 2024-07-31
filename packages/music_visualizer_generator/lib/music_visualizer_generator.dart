7856cfc0913b2b61d193ac77d24a518816dea480
Joanna May <joannamadisonmay@gmail.com>
2024-07-31 17:01:27 -0500

    feat: add music visualizer tool script (#20)
    * feat: add music visualizer tool script
    
    * fix: spelling

---
+++ packages/music_visualizer_generator/lib/music_visualizer_generator.dart
@@ -0,0 +1,10 @@
+/// music_visualizer_generator, A Very Good Project created by Very Good CLI.
+///
+/// ```sh
+/// # activate music_visualizer_generator
+/// dart pub global activate music_visualizer_generator
+///
+/// # see usage
+/// music_visualizer_generator --help
+/// ```
+library;

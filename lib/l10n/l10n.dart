497dda241441e274f6ba2cf2e42f73f1667235e2
albertomontesdeoca <alberto.montesdeoca@verygood.ventures>
2025-09-23 09:17:14 +0200

    Improve translations

--- lib/l10n/l10n.dart
+++ lib/l10n/l10n.dart
@@ -4,6 +4,8 @@
 import 'package:airplane_entertainment_system/l10n/gen/app_localizations.dart';
 import 'package:flutter/widgets.dart';
 
+export 'package:airplane_entertainment_system/l10n/gen/app_localizations.dart';
+
 extension AppLocalizationsX on BuildContext {
   AppLocalizations get l10n => AppLocalizations.of(this);
 }

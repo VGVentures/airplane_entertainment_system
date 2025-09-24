e5573045cb721cee30780b9b0d330d78318a33dd
albertomontesdeoca <alberto.montesdeoca@verygood.ventures>
2025-09-24 08:15:35 +0200

    pr feedback

--- test/airplane_entertainment_system/widgets/mute_button_test.dart
+++ test/airplane_entertainment_system/widgets/mute_button_test.dart
@@ -1,4 +1,4 @@
-// Ignored for improve test readability
+// Ignored to improve test readability
 // ignore_for_file: prefer_const_constructors, avoid_redundant_argument_values
 
 import 'package:airplane_entertainment_system/airplane_entertainment_system/airplane_entertainment_system.dart';
